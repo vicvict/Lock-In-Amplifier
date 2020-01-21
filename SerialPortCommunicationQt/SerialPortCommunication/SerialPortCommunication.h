2b7474f0fa7cf52eb86f49eb4e75c76ecdc588a0
vicvict <38014143+vicvict@users.noreply.github.com>
2020-01-21 14:47:23 +0300

    Add files via upload

---
+++ SerialPortCommunicationQt/SerialPortCommunication/SerialPortCommunication.h
@@ -0,0 +1,11 @@
+#ifndef SERIALPORTCOMMUNICATION_H
+#define SERIALPORTCOMMUNICATION_H
+
+
+#include "SerialPortCommunicationQt.h"
+
+class SerialPortCommunication : public SerialPortCommunicationQt {
+
+};
+
+#endif // SERIALPORTCOMMUNICATION_H
