54f2b6d9e5f3fb7c963bd09f384ec1804705b256
Tsypkin Victor <Tsypkin.VP@phystech.edu>
2020-01-21 18:22:08 +0300

    we can change phase sometimes

--- SerialPortCommunicationQt/SerialPortCommunication/SRS/LockInAmplifier/Final_Lock-In-Amplifier/mainwindow.h
+++ SerialPortCommunicationQt/SerialPortCommunication/SRS/LockInAmplifier/Final_Lock-In-Amplifier/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../../lockinamplifier.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonPhase_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    LockInAmplifier obj;
 };
 
 #endif // MAINWINDOW_H
